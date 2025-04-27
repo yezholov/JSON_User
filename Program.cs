a16e47a2a95aba28dadd6cd346bfe8ca04d9005f
Kyrylo Yezholov <kirill@ezh.com.ua>
2025-04-27 20:20:04 +0300

    Version 2

--- Program.cs
+++ Program.cs
@@ -7,25 +7,35 @@ class Program
     // Define a class to match the JSON structure
     public class User
     {
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int Age { get; set; }
-        public string City { get; set; }
+        public string? City { get; set; }
     }
 
     static void Main(string[] args)
     {
         // Path to the local JSON file
-        string filePath = "user.json";
+        string filePath = "users.json";
 
         // Read the JSON data from the file
         string jsonResponse = File.ReadAllText(filePath);
 
         // Deserialize the JSON into a C# object
-        User user = JsonConvert.DeserializeObject<User>(jsonResponse);
+        List<User>? users = JsonConvert.DeserializeObject<List<User>>(jsonResponse);
 
-        // Display the user's information
-        Console.WriteLine($"Name: {user.Name}");
-        Console.WriteLine($"Age: {user.Age}");
-        Console.WriteLine($"City: {user.City}");
+        if (users != null)
+        {
+            foreach (var user in users)
+            {
+                Console.WriteLine($"Name: {user.Name}");
+                Console.WriteLine($"Age: {user.Age}");
+                Console.WriteLine($"City: {user.City}");
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Failed to deserialize the JSON data into a User object.");
+        }
     }
 }
--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@ Author: Kyrylo Yezholov <@yezholov> <kyrylo.yezholov@sa.stud.vu.lt>
 Version 1:
 - Create .gitignore file
 - Create example JSON reader with JSON file (user.json)
+
+Version 2:
+- Add more users (rename file into users.json)
+- Add loops and update code to read multiply users
