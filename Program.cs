8f36dc2499a1d85a0f23ae2cdf915c531bdb7fbc
Kyrylo Yezholov <kirill@ezh.com.ua>
2025-04-27 20:07:58 +0300

    Version 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+class Program
+{
+    // Define a class to match the JSON structure
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        // Path to the local JSON file
+        string filePath = "user.json";
+
+        // Read the JSON data from the file
+        string jsonResponse = File.ReadAllText(filePath);
+
+        // Deserialize the JSON into a C# object
+        User user = JsonConvert.DeserializeObject<User>(jsonResponse);
+
+        // Display the user's information
+        Console.WriteLine($"Name: {user.Name}");
+        Console.WriteLine($"Age: {user.Age}");
+        Console.WriteLine($"City: {user.City}");
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+Project: [JSOIN_User](https://github.com/yezholov/JSOIN_User)
+
+Author: Kyrylo Yezholov <@yezholov> <kyrylo.yezholov@sa.stud.vu.lt>
